fccf9adf2db4ec998caa6eb773e0f0994f6936f5
KamuiXenom <webmaster@kamuixenom.de>
2015-09-17 09:46:03 +0200

    Snippets
    add snippet SetColor on Lights
    modify comments

--- inGame-Scripts/snippets.cs
+++ inGame-Scripts/snippets.cs
@@ -1,10 +1,22 @@
-// execute an action on group.blocks
+/**
+*	execute an action on group.blocks
+*
+*	@param	list of IMyTerminalBlock all subtypes with actions
+*	@param	action string
+*/
 	void ApplyActionOnGroup( List<IMyTerminalBlock> group, String action ) {
 		for(int i = 0; i < group.Count; ++i){group[i].ApplyAction(action);}
 	}
 
-// door state of group
-	Boolean GetDoorStateOfGroup(List<IMyTerminalBlock> door, Boolean shouldBe) {
+/**
+*	door state of group
+*
+*	@param	list of IMyTerminalBlock subtype IMyDoor
+*	@param	boolean true=Open false=Closed
+*	@return boolean
+*
+*/
+	Boolean GetDoorStateOfGroup( List<IMyTerminalBlock> door, Boolean shouldBe ) {
 		var state = true;
 		for(int i = 0; i < door.Count; ++i) {
 			if(door[i] is IMyDoor) {
@@ -13,4 +25,17 @@
 			}
 		}
 		return state;
+	}
+
+/**
+*	set color on light group
+*
+*	@param	list of IMyTerminalBlock subtype IMyLightingBlock
+*	@param	color definition by Color structure 'VRageMath.Color'
+*/
+	void SetColor(List<IMyTerminalBlock> blocks, Color color) {
+		for( int i = 0; i < blocks.Count; i++ ) {
+			IMyLightingBlock light = blocks[i] as IMyLightingBlock;
+			light.SetValue("Color", color);
+		}
 	}
